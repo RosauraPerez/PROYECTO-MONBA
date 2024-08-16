705a57e4513f7fc45a13975a46436851da4304f7
Rosaura Noemi Perez Elias <perezrosaura136@gmail.com>
2024-08-15 23:45:25 -0600

    Se agregaron los controladores del modulo manifiestos

---
+++ src/java/org/monba/controller/ControllerAcceso.java
@@ -0,0 +1,92 @@
+package org.monba.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.monba.bd.ConexionMySQL;
+import org.monba.model.Usuario;
+
+public class ControllerAcceso {
+
+    public String login(String nombreUsuario, String contrasenia) throws ClassNotFoundException, SQLException, IOException {
+        //1. Definir la consulta
+        String querySELECT = "SELECT * FROM v_usuario WHERE nombre_usuario = ? AND contrasenia = ?";
+
+        String queryUPDATE = "UPDATE usuario SET token = ? WHERE nombre_usuario = ? AND contrasenia = ?;";
+        //2. Establecemos la conexion
+        ConexionMySQL objConMySQL = new ConexionMySQL();
+        try {
+            Connection conn = objConMySQL.abrirConexion();
+            PreparedStatement pstmt = conn.prepareStatement(querySELECT);
+
+            pstmt.setString(1, nombreUsuario);
+            pstmt.setString(2, contrasenia);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String tokenizer = generarToken(nombreUsuario);
+                PreparedStatement updateStatement = conn.prepareStatement(queryUPDATE);
+                int id_empleado = rs.getInt("id_empleado");
+                String nombre = rs.getString("nombre");
+                String a_paterno = rs.getString("a_paterno");
+                String a_materno = rs.getString("a_materno");
+                int idUsuario = rs.getInt("id_usuario");
+                String nombre_usuario = rs.getString("nombre_usuario");
+                String clave = rs.getString("contrasenia");
+                String rol = rs.getString("rol");
+
+                updateStatement.setString(1, tokenizer);
+                updateStatement.setString(2, nombreUsuario);
+                updateStatement.setString(3, contrasenia);
+                updateStatement.executeUpdate();
+
+                Usuario usu = new Usuario(idUsuario, nombre_usuario, clave, rol, tokenizer);
+                return "{"
+                        + "\"id_empleado\":\"" + id_empleado + "\","
+                        + "\"nombre\":\"" + nombre + "\","
+                        + "\"a_paterno\":\"" + a_paterno + "\","
+                        + "\"a_materno\":\"" + a_materno + "\","
+                        + "\"id_usuario\":\"" + usu.getId_usuario() + "\","
+                        + "\"nombre_usuario\":\"" + usu.getNombre_usuario() + "\","
+                        + "\"contrasenia\":\"" + usu.getContrasenia() + "\","
+                        + "\"rol\":\"" + usu.getRol() + "\","
+                        + "\"token\":\"" + usu.getToken() + "\""
+                        + "}";
+
+            } else {
+                return "{\"rol\":\"ERROR\"}";
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return "{\"rol\":\"ERROR\"}";
+    }
+
+    // Método para generar el token
+    public String generarToken(String nombreUsuario) {
+        String token = "";
+        try {
+            Date miFecha = new Date();
+            String fecha = new SimpleDateFormat("dd-MM-yyyy").format(miFecha);
+            String cadena = nombreUsuario + "." + "MONBA" + "." + fecha;
+            token = DigestUtils.md5Hex(cadena);
+            System.out.println("Token generado: " + token);
+        } catch (Exception e) {
+            System.out.println("Error al generar el token: " + e.getMessage());
+        }
+        return token;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ControllerAcceso cl = new ControllerAcceso();
+            System.out.println(cl.login("admin", "admin"));
+        } catch (ClassNotFoundException | SQLException | IOException ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+    }
+}
---
+++ src/java/org/monba/controller/ControllerEmpresa.java
@@ -0,0 +1,207 @@
+package org.monba.controller;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+import org.monba.bd.ConexionMySQL;
+import org.monba.model.Empresa;
+
+public class ControllerEmpresa {
+    public List<Empresa> getAllActivas(String rol) throws SQLException, ClassNotFoundException, IOException {
+        List<Empresa> listaEmpresas = new ArrayList<>();
+        ConexionMySQL connMySQL = new ConexionMySQL();
+
+        // 1. Crear la sentencia SQL
+        String query = "SELECT * FROM empresa WHERE estatus = 1 AND rol = ?";
+        // 2. Se establece la conexión con la BD
+        Connection conn = connMySQL.abrirConexion();
+        // 3. Se genera el statement para enviar la consulta
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, rol);
+        // 4. Se prepara un ResultSet para obtener la respuesta de la BD
+        ResultSet rs = pstmt.executeQuery();
+        // 5. Recorrer el rs y extraer los datos
+        while (rs.next()) {
+            Empresa e = new Empresa();
+            e.setId_empresa(rs.getInt("id_empresa"));
+            e.setNombre(rs.getString("nombre"));
+            e.setDomicilio(rs.getString("domicilio"));
+            e.setColonia(rs.getString("colonia"));
+            e.setCodigo_postal(rs.getString("codigo_postal"));
+            e.setCiudad(rs.getString("ciudad"));
+            e.setEstado(rs.getString("estado"));
+            e.setNum_autorizacion(rs.getString("numero_autorizacion"));
+            e.setTelefono(rs.getString("telefono"));
+            e.setCorreo(rs.getString("correo"));
+            e.setRfc(rs.getString("rfc"));
+            e.setDirector(rs.getString("director"));
+            e.setRol(rs.getString("rol"));
+            e.setEstatus(rs.getInt("estatus"));
+
+            listaEmpresas.add(e);
+        }
+
+        // 6. Cerrar todos los objetos
+        rs.close();
+        pstmt.close();
+        conn.close();
+        connMySQL.cerrarConexion(conn);
+
+        // 7. Devolver la información
+        return listaEmpresas;
+    }
+    
+    public List<Empresa> getAllInactivas(String rol) throws SQLException, ClassNotFoundException, IOException {
+        List<Empresa> listaEmpresas = new ArrayList<>();
+        ConexionMySQL connMySQL = new ConexionMySQL();
+
+        // 1. Crear la sentencia SQL
+        String query = "SELECT * FROM empresa WHERE estatus = 0 AND rol = ?";
+        // 2. Se establece la conexión con la BD
+        Connection conn = connMySQL.abrirConexion();
+        // 3. Se genera el statement para enviar la consulta
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, rol);
+        // 4. Se prepara un ResultSet para obtener la respuesta de la BD
+        ResultSet rs = pstmt.executeQuery();
+        // 5. Recorrer el rs y extraer los datos
+        while (rs.next()) {
+            Empresa e = new Empresa();
+            e.setId_empresa(rs.getInt("id_empresa"));
+            e.setNombre(rs.getString("nombre"));
+            e.setDomicilio(rs.getString("domicilio"));
+            e.setColonia(rs.getString("colonia"));
+            e.setCodigo_postal(rs.getString("codigo_postal"));
+            e.setCiudad(rs.getString("ciudad"));
+            e.setEstado(rs.getString("estado"));
+            e.setNum_autorizacion(rs.getString("numero_autorizacion"));
+            e.setTelefono(rs.getString("telefono"));
+            e.setCorreo(rs.getString("correo"));
+            e.setRfc(rs.getString("rfc"));
+            e.setDirector(rs.getString("director"));
+            e.setRol(rs.getString("rol"));
+            e.setEstatus(rs.getInt("estatus"));
+
+            listaEmpresas.add(e);
+        }
+
+        // 6. Cerrar todos los objetos
+        rs.close();
+        pstmt.close();
+        conn.close();
+        connMySQL.cerrarConexion(conn);
+
+        // 7. Devolver la información
+        return listaEmpresas;
+    }
+    public void activarEmpresa(Empresa e) throws ClassNotFoundException {
+        try {
+            //1. generar la consulta
+            String query = "UPDATE empresa SET estatus = 1 WHERE id_empresa=" + e.getId_empresa()+ ";";
+            //2.Generar la conexion con la BD
+            ConexionMySQL connMysql = new ConexionMySQL();
+            //abrir la conexion
+            Connection conn = connMysql.abrirConexion();
+            //crear objeto que lleva la sentencia - Statement
+            Statement stmt = conn.createStatement();
+            //5.Ejecutar la query
+            stmt.execute(query);
+        } catch (SQLException ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+    }
+    public void eliminarEmpresa(Empresa e) throws ClassNotFoundException {
+        try {
+            //1. generar la consulta
+            String query = "UPDATE empresa SET estatus = 0 WHERE id_empresa=" + e.getId_empresa()+ ";";
+            //2.Generar la conexion con la BD
+            ConexionMySQL connMysql = new ConexionMySQL();
+            //abrir la conexion
+            Connection conn = connMysql.abrirConexion();
+            //crear objeto que lleva la sentencia - Statement
+            Statement stmt = conn.createStatement();
+            //5.Ejecutar la query
+            stmt.execute(query);
+        } catch (SQLException ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+
+    }
+    public int insertarEmpresa(Empresa e) throws SQLException, IOException, ClassNotFoundException {
+        String query = "{call InsertarEmpresa(?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        ConexionMySQL connMysql = new ConexionMySQL();
+        // se abre la conexion
+        Connection conn = connMysql.abrirConexion();
+        // crear el steatment  que llevara la consulta
+        CallableStatement cstm = (CallableStatement) conn.prepareCall(query);
+        //llenar todos los parametros de la llamada al procedure
+        cstm.setString(1, e.getNombre());
+        cstm.setString(2, e.getDomicilio());
+        cstm.setString(3, e.getColonia());
+        cstm.setString(4, e.getCodigo_postal());
+        cstm.setString(5, e.getCiudad());
+        cstm.setString(6, e.getEstado());
+        cstm.setString(7, e.getNum_autorizacion());
+        cstm.setString(8, e.getTelefono());
+        cstm.setString(9, e.getCorreo());
+        cstm.setString(10, e.getRfc());
+        cstm.setString(11, e.getDirector());
+        cstm.setString(12, e.getRol());
+
+        cstm.registerOutParameter(13, Types.INTEGER);  // p_id_empresa
+
+        cstm.execute();
+        
+        e.setId_empresa(cstm.getInt(13));
+        cstm.close();
+        conn.close();
+        connMysql.cerrarConexion(conn);
+        return e.getId_empresa();
+    }
+    public int modificarEmpresa(Empresa e) throws SQLException, IOException, ClassNotFoundException{
+        String query = "{call ModificarEmpresa(?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        ConexionMySQL connMysql = new ConexionMySQL();
+        // se abre la conexion
+        Connection conn = connMysql.abrirConexion();
+        // crear el steatment  que llevara la consulta
+        CallableStatement cstm =  (CallableStatement) conn.prepareCall(query);
+        //llenar todos los parametros de la llamada al procedure
+        cstm.setInt(1, e.getId_empresa());
+        cstm.setString(2, e.getNombre());
+        cstm.setString(3, e.getDomicilio());
+        cstm.setString(4, e.getColonia());
+        cstm.setString(5, e.getCodigo_postal());
+        cstm.setString(6, e.getCiudad());
+        cstm.setString(7, e.getEstado());
+        cstm.setString(8, e.getNum_autorizacion());
+        cstm.setString(9, e.getTelefono());
+        cstm.setString(10, e.getCorreo());
+        cstm.setString(11, e.getRfc());
+        cstm.setString(12, e.getDirector());
+        cstm.setString(13, e.getRol());
+        
+        cstm.execute();
+        
+        cstm.close();
+        conn.close();
+        connMysql.cerrarConexion(conn);
+        
+        return e.getId_empresa();
+    }
+    public static void main(String[] args) {
+        try {
+            ControllerEmpresa ce = new ControllerEmpresa();
+            Empresa e = new Empresa(1, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 1);
+            System.out.println(ce.modificarEmpresa(e));
+        } catch (SQLException | IOException | ClassNotFoundException ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+    }
+}
